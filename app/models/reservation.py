7b08ffcf08a08b9647a611fbfca3dfb74e28e6a6
Lymar Volodymyr <shaxtar11@gmail.com>
2024-11-01 22:35:09 +0100

    Add user relationship to Reservation. Create migration.

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -7,6 +7,7 @@ class Reservation(Base):
     from_reserve = Column(DateTime)
     to_reserve = Column(DateTime)
     meetingroom_id = Column(Integer, ForeignKey('meetingroom.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Already booked from {self.from_reserve} to {self.to_reserve}'
