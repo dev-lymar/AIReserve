3302d260c67a493118a49614b43421bd7e4341de
Lymar Volodymyr <shaxtar11@gmail.com>
2024-11-01 22:30:25 +0100

    Create migration for User model.

--- app/core/base.py
+++ app/core/base.py
@@ -1,4 +1,4 @@
 """Base class import and all models for Alembic."""
 
 from app.core.db import Base  # noqa
-from app.models.meeting_room import MeetingRoom, Reservation, User  # noqa
+from app.models import MeetingRoom, Reservation, User  # noqa
